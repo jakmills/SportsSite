c6eb7ec6fdcd4a1e8b4cdc3d5883b728bb51a8f1
Yash Patel <ypatel41805@gmail.com>
2025-11-14 18:38:54 -0500

    added dotenv dependency and accessed api key through .env file, also made 2 new classes

--- BackEnd/jashsports/src/main/java/com/jash/Main.java
+++ BackEnd/jashsports/src/main/java/com/jash/Main.java
@@ -1,7 +1,13 @@
 package com.jash;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        Dotenv dotnev = Dotenv.load();
+        String apikey = dotnev.get("API_KEY");
+
+        System.out.println(apikey);
     }
 }
---
+++ BackEnd/jashsports/src/main/java/com/jash/footballDATA.java
@@ -0,0 +1,5 @@
+package com.jash;
+
+public class footballDATA {
+    
+}
