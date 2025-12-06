82f1e48f369b64fb6e5844c3e2503894ab09735e
ypatel418 <ypatel41805@gmail.com>
2025-12-05 20:40:41 -0500

    Styling changes to home page

--- BackEnd/jashsports/src/main/java/com/jash/footballDATA/footballDATA.java
+++ BackEnd/jashsports/src/main/java/com/jash/footballDATA/footballDATA.java
@@ -1,4 +1,5 @@
 package com.jash.footballDATA;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -6,42 +7,43 @@ import com.google.gson.Gson;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-import io.github.cdimascio.dotenv.Dotenv; 
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class footballDATA {
 
-    public footballDATA() {   
+    public footballDATA() {
     }
-    
-    private String getApikey(){
+
+    private String getApikey() {
         Dotenv dotnev = Dotenv.load();
         return dotnev.get("FOOTBALLDATA_API_KEY");
     }
 
-     public ArrayList<Match> getMatches(String league) throws Exception {
+    public ArrayList<Match> getMatches(String league) throws Exception {
         try {
-        String apiKey = getApikey();
-        OkHttpClient client = new OkHttpClient();
-        LocalDate today = LocalDate.now();
-        Request request = new Request.Builder()
-            .url("https://api.football-data.org/v4/competitions/" + getLeagueID(league) + "/matches?dateFrom="+ today.toString() +"&dateTo="+ today.toString())
-            .addHeader("X-Auth-Token", apiKey)
-            .build();
-        Response response = client.newCall(request).execute();
-        if (!response.isSuccessful()) {
-            throw new RuntimeException("API request failed: " + response.code());
-        } 
-        String json = response.body().string();
-        Gson gson = new Gson();     // Parse the top-level object
-        MatchesResponse data = gson.fromJson(json, MatchesResponse.class);
-        return data.matches; // This is your ArrayList<Match>
+            String apiKey = getApikey();
+            OkHttpClient client = new OkHttpClient();
+            LocalDate today = LocalDate.now();
+            Request request = new Request.Builder()
+                    .url("https://api.football-data.org/v4/competitions/" + getLeagueID(league) + "/matches?dateFrom="
+                            + today.toString() + "&dateTo=" + today.toString())
+                    .addHeader("X-Auth-Token", apiKey)
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("API request failed: " + response.code());
+            }
+            String json = response.body().string();
+            Gson gson = new Gson(); // Parse the top-level object
+            MatchesResponse data = gson.fromJson(json, MatchesResponse.class);
+            return data.matches; // This is your ArrayList<Match>
         } catch (Exception e) {
             return null;
         }
     }
 
     public int getLeagueID(String league) {
-        switch (league.toLowerCase().replaceAll("\\s", "")){
+        switch (league.toLowerCase().replaceAll("\\s", "")) {
             case "championsleague":
                 return 2001;
             case "premierleague":
