d82bf8256735e91717f2836231d17bdcd37265bf
jakmills <jakem0822@outlook.com>
2025-12-14 09:30:17 -0500

    Made styling changes to the search bar, also make it so when you press enter it searches for a team

--- BackEnd/jashsports/src/main/java/com/jash/database/database.java
+++ BackEnd/jashsports/src/main/java/com/jash/database/database.java
@@ -4,23 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class database {
 
-    //private static Dotenv dotenv = Dotenv.load();
+    private static Dotenv dotenv = Dotenv.load();
 
     // Gets DB credentials from .env file
-    // private static String DB_URL = dotenv.get("DB_URL");
-    // private static String DB_USERNAME = dotenv.get("DB_USERNAME");
-    // private static String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+     private static String DB_URL = dotenv.get("DB_URL");
+     private static String DB_USERNAME = dotenv.get("DB_USERNAME");
+     private static String DB_PASSWORD = dotenv.get("DB_PASSWORD");
 
-    private String DB_URL;
-    private String DB_USERNAME;
-    private String DB_PASSWORD;
+    // private String DB_URL;
+    // private String DB_USERNAME;
+    // private String DB_PASSWORD;
 
     public database() {
-        DB_URL = System.getenv("DB_URL");
-        DB_USERNAME = System.getenv("DB_USERNAME");
-        DB_PASSWORD = System.getenv("DB_PASSWORD");
+        // DB_URL = System.getenv("DB_URL");
+        // DB_USERNAME = System.getenv("DB_USERNAME");
+        // DB_PASSWORD = System.getenv("DB_PASSWORD");
 
         if (DB_URL == null || DB_USERNAME == null || DB_PASSWORD == null) {
             throw new IllegalStateException("Database environment variables are not set");
